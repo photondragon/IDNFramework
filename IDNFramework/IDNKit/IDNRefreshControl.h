32712a3b71759bda94988ec85b2683d672631a3a
photondragon <photondragon@163.com>
2015-05-17 13:03:07 +0800

    new file:   IDNFramework/IDNKit/IDNRefreshControl.h new file:   IDNFramework/IDNKit/IDNRefreshControl.m new file:   IDNFramework/IDNKit/UIViewController+IDNPrompt.h new file:   IDNFramework/IDNKit/UIViewController+IDNPrompt.m

---
+++ IDNFramework/IDNKit/IDNRefreshControl.h
@@ -0,0 +1,38 @@
+//
+//  IDNRefreshControl.h
+//  IDNFramework
+//
+//  Created by photondragon on 15/5/17.
+//  Copyright (c) 2015年 iosdev.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 用于显示上拉刷新、下拉显示更多的控件
+// 当用户拖动触发刷新的时候，控件会发出ValueChanged事件。
+// 控件不会自动进入刷新状态，需要手动设置refreshing=YES
+// 设置hidden=YES或enable=NO就不会触发此事件。
+@interface IDNRefreshControl : UIControl
+
+@property (nonatomic,weak) UIScrollView* containerView;//当设置这个属性时，刷新控件就会自动加入这个containerView
+
+@property (nonatomic) BOOL refreshing;
+
+@property (nonatomic) float maxPullingDistance; //拉到这个距离就显示“松开加载更多”。默认值80.0，不得小于50.0
+@property (nonatomic) float minPullingDistance; //拉到这个距离就显示百分比。默认值30.0，不得小于20
+@property (nonatomic) float pulledDistance; //已经拉的距离，通过设置这个属性控制控件的状态。pulledDistance>=maxPullingDistance表示拉到位了。如果设置一个负数，将被改为0
+@property (nonatomic,readonly) BOOL isMaxPulling;
+
+@property (nonatomic,copy) NSAttributedString* normalTitle; //正常状态下显示的文本。默认@"下拉显示更多"
+@property (nonatomic,copy) NSAttributedString* pullingTitle; //正在拉状态下显示的文本。默认@"下拉显示更多"
+@property (nonatomic,copy) NSAttributedString* pulledTitle; //拉到位状态下显示的文本。默认@"松开加载更多"
+@property (nonatomic,copy) NSAttributedString* refreshingTitle; //正在刷新状态下显示的文本。默认@"正在加载更多"
+
+- (instancetype)initAtBottom:(BOOL)atBottom; //刷新控件就放在容器的顶部还是底部（下拉刷新还是上拉加载更多）
+
+@end
+
+@interface UITableView(IDNRefreshControl)
+@property(nonatomic,strong,readonly) IDNRefreshControl* topRefreshControl;//首次访问此属性时，会在tableView中创建刷新控件，无法移除，只能设置topRefreshControl.hidden = YES将其隐藏。
+@property(nonatomic,strong,readonly) IDNRefreshControl* bottomRefreshControl;//首次访问此属性时，会在tableView中创建刷新控件，无法移除，只能设置bottomRefreshControl.hidden = YES将其隐藏。
+@end
---
+++ IDNFramework/IDNKit/IDNRefreshControl.m
@@ -0,0 +1,535 @@
+//
+//  IDNRefreshControl.m
+//  IDNFramework
+//
+//  Created by photondragon on 15/5/17.
+//  Copyright (c) 2015年 iosdev.net. All rights reserved.
+//
+
+#import "IDNRefreshControl.h"
+#import <objc/runtime.h>
+
+//弧度转角度
+#define RADIANS_TO_DEGREES(radians) ((radians) * (180.0 / M_PI))
+//角度转弧度
+#define DEGREES_TO_RADIANS(angle) ((angle) / 180.0 * M_PI)
+
+#define TextColor [UIColor colorWithWhite:0.6 alpha:1.0]
+#define CakeColor [UIColor colorWithWhite:0.7 alpha:1.0]
+
+#define RefreshControlHeight 60 //刷新控件的默认高度
+#define ProgressViewSize 20 //进度提示View（饼视图）的大小
+
+enum IDNRefreshPullState
+{
+	IDNRefreshControlStateNormal=0, //正常状态。显示“下拉显示更多”
+	IDNRefreshControlStatePulling, //正在拉，还没拉到位。可以显示“下拉显示更多”+进度条（显示是否拉到位）
+	IDNRefreshControlStatePulled, //拉到位状态。可以显示“松开加载更多”+进度条（100%）
+};
+
+//指示拉的进度的饼状视图。
+@interface IDNRefreshControlCakeView : UIView
+@property(nonatomic) float ratio; //比例[0, 1.0]
+@property(nonatomic,copy) UIColor* color; //饼的颜色
+@end
+@implementation IDNRefreshControlCakeView
+
+- (CGSize)intrinsicContentSize
+{
+	return CGSizeMake(ProgressViewSize, ProgressViewSize);
+}
+
+- (void)setRatio:(float)ratio
+{
+	if(ratio<0)
+		ratio = 0;
+	else if(ratio>1.0)
+		ratio = 1.0;
+	if(_ratio == ratio)
+		return;
+	_ratio = ratio;
+	[self setNeedsDisplay];
+}
+- (void)drawRect:(CGRect)rect
+{
+	CGRect frame = self.frame;
+	UIBezierPath* path = [UIBezierPath bezierPath];//WithRect:CGRectMake(0, 0, 1.0, 1.0)];
+	[path addArcWithCenter:CGPointMake(0.5, 0.5)
+					radius:0.5
+				startAngle:DEGREES_TO_RADIANS(-90)
+				  endAngle:DEGREES_TO_RADIANS(-90+360.0*self.ratio)
+				 clockwise:YES];
+	[path addLineToPoint:CGPointMake(0.5, 0.5)];
+	[path addLineToPoint:CGPointMake(0.5, 0)];
+	[path applyTransform:CGAffineTransformMakeScale(frame.size.width, frame.size.height)];
+	[_color setFill];
+	[path fill];
+}
+@end
+
+@interface IDNRefreshControl()
+{
+	BOOL isAtBottom;
+	CGFloat addedInsetLength;
+}
+
+@property(nonatomic) enum IDNRefreshPullState pullState;
+@property(nonatomic, weak) IDNRefreshControlCakeView* cakeView;
+@property(nonatomic, weak) UILabel* pullLabelView;
+@property(nonatomic, weak) UILabel* loadingLabelView;
+@property(nonatomic, weak) UIActivityIndicatorView* loadingView; //旋转菊花
+
+@end
+@implementation IDNRefreshControl
+
+- (void)initialize
+{
+	if(self.cakeView==nil)
+	{
+		//不采用autoLayout方案，因为ios7.1上的tableView的实现有Bug，当向其加入subview时，如果用autolayout，就会出异常。只能用autoresizingMask
+		self.autoresizingMask = UIViewAutoresizingFlexibleWidth;
+		//self.translatesAutoresizingMaskIntoConstraints = NO;
+
+		_maxPullingDistance = 80.0;
+		_minPullingDistance = 30.0;
+
+		if(isAtBottom)
+		{
+			self.normalTitle = [[NSAttributedString alloc] initWithString:@"加载更多"];
+			self.pullingTitle = [[NSAttributedString alloc] initWithString:@"加载更多"];
+			self.pulledTitle = [[NSAttributedString alloc] initWithString:@"松开加载"];
+			self.refreshingTitle = [[NSAttributedString alloc] initWithString:@"正在加载"];
+		}
+		else
+		{
+			self.normalTitle = [[NSAttributedString alloc] initWithString:@"下拉刷新"];
+			self.pullingTitle = [[NSAttributedString alloc] initWithString:@"下拉刷新"];
+			self.pulledTitle = [[NSAttributedString alloc] initWithString:@"松开刷新"];
+			self.refreshingTitle = [[NSAttributedString alloc] initWithString:@"正在刷新"];
+		}
+
+		IDNRefreshControlCakeView* cakeView = [[IDNRefreshControlCakeView alloc] init];//WithFrame:CGRectMake(0, 0, 30, 30)];
+		cakeView.translatesAutoresizingMaskIntoConstraints = NO;
+		cakeView.backgroundColor = [UIColor clearColor];
+		cakeView.color = CakeColor;
+		[self addSubview:cakeView];
+		self.cakeView = cakeView;
+
+		UIActivityIndicatorView* loadingView = [[UIActivityIndicatorView alloc] init];//WithFrame:CGRectMake(0, 0, 30, 30)];
+		loadingView.translatesAutoresizingMaskIntoConstraints = NO;
+		loadingView.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+		loadingView.hidden = YES;
+		[self addSubview:loadingView];
+		self.loadingView = loadingView;
+
+		UIFont* font = [UIFont systemFontOfSize:16];
+		UILabel* pullLabelView = [[UILabel alloc] initWithFrame:CGRectMake(30, 0, 290, 30)];
+		pullLabelView.textColor = TextColor;
+		pullLabelView.font = font;
+		pullLabelView.translatesAutoresizingMaskIntoConstraints = NO;
+		pullLabelView.attributedText = self.normalTitle;
+		[self addSubview:pullLabelView];
+		self.pullLabelView = pullLabelView;
+
+		UILabel* refreshingLabelView = [[UILabel alloc] initWithFrame:CGRectMake(30, 0, 290, 30)];
+		refreshingLabelView.textColor = TextColor;
+		refreshingLabelView.font = font;
+		refreshingLabelView.translatesAutoresizingMaskIntoConstraints = NO;
+		refreshingLabelView.attributedText = self.refreshingTitle;
+		refreshingLabelView.hidden = YES;
+		[self addSubview:refreshingLabelView];
+		self.loadingLabelView = refreshingLabelView;
+
+		// 子控件垂直居中
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:cakeView attribute:NSLayoutAttributeCenterY
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterY
+														multiplier:1.0 constant:0]];
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:loadingView attribute:NSLayoutAttributeCenterY
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterY
+														multiplier:1.0 constant:0]];
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:pullLabelView attribute:NSLayoutAttributeCenterY
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterY
+														multiplier:1.0 constant:0]];
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:refreshingLabelView attribute:NSLayoutAttributeCenterY
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterY
+														multiplier:1.0 constant:0]];
+
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:cakeView attribute:NSLayoutAttributeLeading
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterX
+														multiplier:1.0 constant:-60]];
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:loadingView attribute:NSLayoutAttributeLeading
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterX
+														multiplier:1.0 constant:-60]];
+
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:pullLabelView attribute:NSLayoutAttributeLeading
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterX
+														multiplier:1.0 constant:-30]];
+		[self addConstraint:[NSLayoutConstraint constraintWithItem:refreshingLabelView attribute:NSLayoutAttributeLeading
+														 relatedBy:NSLayoutRelationEqual
+															toItem:self attribute:NSLayoutAttributeCenterX
+														multiplier:1.0 constant:-30]];
+
+		//		// 控件高度约束
+		//		NSLayoutConstraint* constraintHeight = [NSLayoutConstraint constraintWithItem:self attribute:NSLayoutAttributeHeight
+		//																 relatedBy:NSLayoutRelationEqual
+		//																	toItem:nil attribute:NSLayoutAttributeNotAnAttribute
+		//																multiplier:0 constant:RefreshControlHeight];
+		//		[self addConstraint:constraintHeight];
+		//		[self setNeedsLayout];
+	}
+}
+
+- (instancetype)initAtBottom:(BOOL)atBottom
+{
+	self = [super initWithFrame:CGRectZero];
+	if (self) {
+		isAtBottom = atBottom;
+		[self initialize];
+	}
+	return self;
+}
+
+- (instancetype)init
+{
+	self = [super initWithFrame:CGRectZero];
+	if (self) {
+		[self initialize];
+	}
+	return self;
+}
+- (instancetype)initWithFrame:(CGRect)frame
+{
+	self = [super initWithFrame:frame];
+	if (self) {
+		[self initialize];
+	}
+	return self;
+}
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+	self = [super initWithCoder:aDecoder];
+	if (self) {
+		[self initialize];
+	}
+	return self;
+}
+
+- (BOOL)isMaxPulling
+{
+	return _pulledDistance>=_maxPullingDistance;
+}
+- (void)setMaxPullingDistance:(float)maxPullingDistance
+{
+	if(maxPullingDistance<RefreshControlHeight)
+		maxPullingDistance = RefreshControlHeight;
+	if(maxPullingDistance<_minPullingDistance+30)
+		maxPullingDistance = _minPullingDistance+30;
+	if(_maxPullingDistance==maxPullingDistance)
+		return;
+	_maxPullingDistance = maxPullingDistance;
+	[self updatePercent];
+}
+- (void)setMinPullingDistance:(float)minPullingDistance
+{
+	if(minPullingDistance<20.0)
+		minPullingDistance = 20.0;
+	if(minPullingDistance>_maxPullingDistance-30)
+		minPullingDistance = _maxPullingDistance-30;
+	if(_minPullingDistance==minPullingDistance)
+		return;
+	_minPullingDistance = minPullingDistance;
+	[self updatePercent];
+}
+- (void)setPulledDistance:(float)pulledDistance
+{
+	if(pulledDistance<0)
+		pulledDistance = 0;
+	if(_pulledDistance == pulledDistance)
+		return;
+	_pulledDistance = pulledDistance;
+	[self updatePercent];
+}
+- (void)updatePercent
+{
+	float ratio = (_pulledDistance-_minPullingDistance)/(_maxPullingDistance-_minPullingDistance);
+	if(ratio<0)
+		ratio = 0;
+	self.cakeView.ratio = ratio;
+	if(ratio<=0)
+		self.pullState = IDNRefreshControlStateNormal;
+	if(ratio>=1.0)
+		self.pullState = IDNRefreshControlStatePulled;
+	else
+		self.pullState = IDNRefreshControlStatePulling;
+}
+
+- (void)setNormalTitle:(NSAttributedString *)normalTitle
+{
+	if([_normalTitle isEqualToAttributedString:normalTitle])
+		return;
+	_normalTitle = normalTitle;
+	if(self.pullState==IDNRefreshControlStateNormal)
+		self.pullLabelView.attributedText = normalTitle;
+}
+
+- (void)setPullingTitle:(NSAttributedString *)pullingTitle
+{
+	if([_pullingTitle isEqualToAttributedString:pullingTitle])
+		return;
+	_pullingTitle = pullingTitle;
+	if(self.pullState==IDNRefreshControlStatePulling)
+		self.pullLabelView.attributedText = pullingTitle;
+}
+- (void)setPulledTitle:(NSAttributedString *)pulledTitle
+{
+	if([_pulledTitle isEqualToAttributedString:pulledTitle])
+		return;
+	_pulledTitle = pulledTitle;
+	if(self.pullState==IDNRefreshControlStatePulled)
+		self.pullLabelView.attributedText = pulledTitle;
+}
+- (void)setRefreshingTitle:(NSAttributedString *)refreshingTitle
+{
+	if([_refreshingTitle isEqualToAttributedString:refreshingTitle])
+		return;
+	_refreshingTitle = refreshingTitle;
+	self.loadingLabelView.attributedText = refreshingTitle;
+}
+
+- (void)setRefreshing:(BOOL)refreshing
+{
+	if(_refreshing==refreshing)
+		return;
+	_refreshing = refreshing;
+	if(refreshing)//进入刷新状态
+	{
+		self.cakeView.hidden = YES;
+		self.pullLabelView.hidden = YES;
+		self.loadingView.hidden = NO;
+		self.loadingLabelView.hidden = NO;
+		[self.loadingView startAnimating];
+
+		if(addedInsetLength==0)
+		{
+			addedInsetLength = self.pulledDistance;
+			if(addedInsetLength<40)
+				addedInsetLength = 40;
+			UIEdgeInsets inset = self.containerView.contentInset;
+			if(isAtBottom)
+				inset.bottom += addedInsetLength;
+			else
+				inset.top += addedInsetLength;
+			self.containerView.contentInset = inset;
+		}
+	}
+	else//退出刷新状态
+	{
+		self.cakeView.hidden = NO;
+		self.pullLabelView.hidden = NO;
+		self.loadingView.hidden = YES;
+		self.loadingLabelView.hidden = YES;
+		[self.loadingView stopAnimating];
+
+		UIEdgeInsets inset = self.containerView.contentInset;
+		if(addedInsetLength>0)
+		{
+			if(isAtBottom)
+				inset.bottom -= addedInsetLength;
+			else
+				inset.top -= addedInsetLength;
+			addedInsetLength = 0;
+
+			UIScrollView* containerView = self.containerView;
+			if (isAtBottom) //加载更多
+			{
+				CGPoint offset = containerView.contentOffset;
+				[containerView setContentOffset:offset animated:YES];//让contentView立刻停止滚动，否则改变inset后界面可能会产生跳变。
+
+				containerView.contentInset = inset;
+
+				CGSize frameSize = containerView.frame.size;
+				CGSize contentSize = containerView.contentSize;
+				CGFloat maxOffset = contentSize.height-frameSize.height;
+				if(maxOffset<0)
+					maxOffset = 0;
+
+				if(offset.y<0)
+					offset.y = 0;
+				else if(offset.y>maxOffset)
+					offset.y = maxOffset;
+				containerView.contentOffset = offset; //改变底部inset后，保持contentOffset不变，避免界面发生跳动
+			}
+			else
+			{
+				[containerView setContentOffset:containerView.contentOffset animated:YES];//让contentView立刻停止滚动，否则改变inset后界面可能会产生跳变。
+				[UIView animateWithDuration:0.3 animations:^{
+					containerView.contentInset = inset;
+				}];
+			}
+		}
+	}
+}
+
+- (void)setPullState:(enum IDNRefreshPullState)mkstate
+{
+	if(_pullState==mkstate)
+		return;
+	_pullState = mkstate;
+	if(_pullState==IDNRefreshControlStateNormal)
+		self.pullLabelView.attributedText = self.normalTitle;
+	else if(_pullState==IDNRefreshControlStatePulling)
+		self.pullLabelView.attributedText = self.pullingTitle;
+	else if(_pullState==IDNRefreshControlStatePulled)
+		self.pullLabelView.attributedText = self.pulledTitle;
+}
+
+- (void)dragEnded
+{
+	if (self.enabled == YES && self.hidden==NO && self.refreshing==NO && self.isMaxPulling) {
+		[self sendActionsForControlEvents:UIControlEventValueChanged];
+	}
+}
+
+- (void)willMoveToWindow:(UIWindow *)newWindow
+{
+	//之所以在这里注册/删除KVO，是因为当[containerView dealloc]时不会自动删除KVO，导致程序崩溃。本来这些代码是在setContainerView:中的。
+	//不在能willMoveToSuperview:nil中实现这些功能。因为函数的调用顺序是[self willMoveToWindow:nil], [containerView dealloc], [self willMoveToSuperview:nil]
+	//在[containerView dealloc]之后是无法删除KVO的
+	if(newWindow)//加入
+	{
+		if(isAtBottom)//上拉加载更多控件
+			[_containerView addObserver:self forKeyPath:@"contentSize" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+		[_containerView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:nil];
+	}
+	else
+	{
+		if(isAtBottom)
+			[_containerView removeObserver:self forKeyPath:@"contentSize"];
+		[_containerView removeObserver:self forKeyPath:@"contentOffset"];
+	}
+}
+
+- (void)setContainerView:(UIScrollView *)containerView
+{
+	if(_containerView==containerView)
+		return;
+	if(_containerView)
+	{
+		[_containerView.panGestureRecognizer removeTarget:self action:@selector(panGestureRecognizerStateUpdate:)];
+		[self removeFromSuperview];
+	}
+	_containerView = containerView;
+	if(containerView==nil)//新containerView为空
+		return;
+
+	[containerView.panGestureRecognizer addTarget:self action:@selector(panGestureRecognizerStateUpdate:)];
+
+	UIEdgeInsets inset = containerView.contentInset;
+	if(isAtBottom)//上拉加载更多控件
+	{
+		CGRect frame = CGRectMake(0, containerView.contentSize.height+inset.top+inset.bottom, containerView.frame.size.width, RefreshControlHeight);
+		if(frame.origin.y<containerView.frame.size.height)
+			frame.origin.y = containerView.frame.size.height;
+		self.frame = frame;
+	}
+	else//下拉刷新控件
+		self.frame = CGRectMake(0, -RefreshControlHeight-inset.top, containerView.frame.size.width, RefreshControlHeight);
+	[containerView addSubview:self];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+					  ofObject:(id)object
+						change:(NSDictionary *)change
+					   context:(void *)context
+{
+
+	if([keyPath isEqualToString:@"contentSize"])
+	{
+		NSValue* sizeValue = [change objectForKey:NSKeyValueChangeNewKey];
+		CGRect frame = self.frame;
+		frame.origin.y = [sizeValue CGSizeValue].height;
+		if(frame.origin.y<_containerView.frame.size.height)
+			frame.origin.y = _containerView.frame.size.height;
+		self.frame = frame;
+	}
+	else if([keyPath isEqualToString:@"contentOffset"])
+	{
+		CGFloat offset = self.containerView.contentOffset.y;
+
+		if(isAtBottom)
+		{
+			CGFloat maxOffset = self.containerView.contentSize.height - self.containerView.frame.size.height;
+			if(maxOffset<0)
+				maxOffset = 0;
+			CGFloat pulled = offset - maxOffset;
+			if(pulled<0)
+				pulled = 0;
+			self.pulledDistance = pulled;
+		}
+		else
+		{
+			CGFloat pulledDistance = -offset;
+			if(pulledDistance<0)
+				pulledDistance = 0;
+			self.pulledDistance = pulledDistance;
+		}
+	}
+}
+
+- (void)panGestureRecognizerStateUpdate:(UIGestureRecognizer*)recognizer
+{
+	if(recognizer.state == UIGestureRecognizerStateEnded)//相当于containerView的dragging end
+		[self dragEnded];
+}
+
+@end
+
+static char associatedObjectKeyIDNRefreshControl = 0;
+
+@implementation UITableView(IDNRefreshControl)
+
+- (NSMutableDictionary*)dictionaryOfIDNRefreshControl
+{
+	NSMutableDictionary* dic = objc_getAssociatedObject(self, &associatedObjectKeyIDNRefreshControl);
+	if(dic==nil)
+	{
+		dic = [NSMutableDictionary dictionary];
+		objc_setAssociatedObject(self, &associatedObjectKeyIDNRefreshControl, dic, OBJC_ASSOCIATION_RETAIN);
+	}
+	return dic;
+}
+
+- (IDNRefreshControl*)topRefreshControl
+{
+	NSMutableDictionary* dic = [self dictionaryOfIDNRefreshControl];
+	IDNRefreshControl* refreshControl = dic[@"topRefreshControl"];
+	if(refreshControl==nil)
+	{
+		IDNRefreshControl* refreshControl = [[IDNRefreshControl alloc] init];
+		refreshControl.containerView = self;
+		dic[@"topRefreshControl"] = refreshControl;
+	}
+	return refreshControl;
+}
+
+- (IDNRefreshControl*)bottomRefreshControl
+{
+	NSMutableDictionary* dic = [self dictionaryOfIDNRefreshControl];
+	IDNRefreshControl* refreshControl = dic[@"bottomRefreshControl"];
+	if(refreshControl==nil)
+	{
+		IDNRefreshControl* refreshControl = [[IDNRefreshControl alloc] initAtBottom:YES];
+		refreshControl.containerView = self;
+		dic[@"bottomRefreshControl"] = refreshControl;
+	}
+	return refreshControl;
+}
+
+@end
